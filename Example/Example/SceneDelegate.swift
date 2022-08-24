ba0e987b27a0c66646e2fb7bf7096bde955ec21a
Lucille Benoit <lucillebenoit1@gmail.com>
2022-08-24 16:26:35 -0700

    update scene delegate path

---
+++ Example/Example/SceneDelegate.swift
@@ -0,0 +1,30 @@
+// swiftlint:disable line_length
+//
+//  SceneDelegate.swift
+//  Example
+//
+//  Created by Lucille Benoit on 8/22/22.
+//
+
+import UIKit
+
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+
+        let viewController = ViewController()
+        let navigation = UINavigationController(rootViewController: viewController)
+
+        window.rootViewController = navigation
+
+        self.window = window
+        window.makeKeyAndVisible()
+
+    }
+}
