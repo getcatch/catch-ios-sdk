06826a08542a1490880be16c0bc467deef416e1c
Lucille Benoit <lucillebenoit1@gmail.com>
2022-10-07 15:19:37 -0700

    update font documentation

--- Sources/Catch/Utilities/CatchFont.swift
+++ Sources/Catch/Utilities/CatchFont.swift
@@ -33,40 +33,40 @@ enum CatchFont {
 }
 
 extension CatchFont {
-    /// Circular Bold 26 point Font
+    /// Circular Bold 26 point font
     static let heading1 = getCatchFont(name: FontName.bold, size: 26)
 
-    /// Circular Bold 26 point Font
+    /// Circular Bold 20 point font
     static let heading2 = getCatchFont(name: FontName.bold, size: 20)
 
-    /// Circular Bold 26 point Font
+    /// Circular Bold 16 point font
     static let heading3 = getCatchFont(name: FontName.bold, size: 16)
 
-    /// Circular Bold 26 point Font
+    /// Circular Bold 14 point font
     static let heading4 = getCatchFont(name: FontName.bold, size: 14)
 
-    /// Circular Regular 18 point Font
+    /// Circular Regular 18 point font
     static let bodyLarge = getCatchFont(name: FontName.regular, size: 18)
 
-    /// Circular Regular 16 point Font
+    /// Circular Regular 16 point font
     static let bodyRegular = getCatchFont(name: FontName.regular, size: 16)
 
-    /// Circular Regular 14 point Font
+    /// Circular Regular 14 point font
     static let bodySmall = getCatchFont(name: FontName.regular, size: 14)
 
-    /// Circular Medium 18 point Font
+    /// Circular Medium 18 point font
     static let linkLarge = getCatchFont(name: FontName.medium, size: 18)
 
-    /// Circular Medium 16 point Font
+    /// Circular Medium 16 point font
     static let linkRegular = getCatchFont(name: FontName.medium, size: 16)
 
-    /// Circular Medium 14 point Font
+    /// Circular Medium 14 point font
     static let linkSmall = getCatchFont(name: FontName.medium, size: 14)
 
-    /// Circular Medium 18 point Font
+    /// Circular Medium 18 point font
     static let buttonLabel = getCatchFont(name: FontName.medium, size: 18)
 
-    /// Circular Medium 16 point Font
+    /// Circular Medium 16 point font
     static let buttonLabelCompact = getCatchFont(name: FontName.medium, size: 16)
 
     private static func getCatchFont(name: FontName, size: CGFloat) -> UIFont {
