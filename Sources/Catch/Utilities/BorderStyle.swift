061418a4f20dc1eb6d795db6540520e1a77f3f00
Lucille Benoit <lucillebenoit1@gmail.com>
2022-10-07 14:27:13 -0700

    add border styles and common UI layout constants

---
+++ Sources/Catch/Utilities/BorderStyle.swift
@@ -0,0 +1,41 @@
+//
+//  BorderStyle.swift
+//  Catch
+//
+//  Created by Lucille Benoit on 9/7/22.
+//
+
+import UIKit
+
+public enum BorderStyle {
+    /// Renders a border with slightly rounded rect corners.
+    case roundedRect
+    /// Renders a border with fully rounded corners.
+    case pill
+    /// Renders a border with square corners.
+    case square
+    /// Renders widgets with no borders and no padding around the internal content.
+    case none
+
+    /**
+     Calculates the cornerRadius for the border style based on the view height
+     and whether or not the widget is one of the larger-height widgets.
+     - Parameter viewHeight: The height of the widget.
+     - Parameter isLargeElement: Should be true for PurchaseConfirmation
+     and CampaignLink widgets. Defaults to false.
+     */
+    internal func cornerRadius(for viewHeight: CGFloat, isLargeElement: Bool = false) -> CGFloat? {
+        switch self {
+        case .roundedRect:
+            return isLargeElement
+            ? UIConstant.largeCornerRadius
+            : UIConstant.defaultCornerRadius
+        case .pill:
+            return viewHeight / 2
+        case .square:
+            return 0
+        case .none:
+            return nil
+        }
+    }
+}
---
+++ Sources/Catch/Utilities/UIConstant.swift
@@ -0,0 +1,43 @@
+//
+//  UIConstant.swift
+//  Catch
+//
+//  Created by Lucille Benoit on 9/7/22.
+//
+
+import UIKit
+
+enum UIConstant {
+    /// 2 point spacing
+    static let microSpacing: CGFloat = 2
+
+    /// 3 point spacing
+    static let extraSmallSpacing: CGFloat = 3
+
+    /// 4 point spacing
+    static let smallSpacing: CGFloat = 4
+
+    /// 8 point spacing
+    static let mediumSpacing: CGFloat = 8
+
+    /// 16 point spacing
+    static let largeSpacing: CGFloat = 16
+
+    /// 24 point spacing
+    static let extraLargeSpacing: CGFloat = 24
+
+    /// 20 point default text line height
+    static let defaultLineHeight: CGFloat = 20
+
+    /// 24 point larger text line height
+    static let largeLineHeight: CGFloat = 24
+
+    /// 4 point default corner radius for borders
+    static let defaultCornerRadius: CGFloat = 4
+
+    /// 5 point large corner radius for borders
+    static let largeCornerRadius: CGFloat = 5
+
+    /// 1 point default width for borders
+    static let defaultBorderWidth: CGFloat = 1
+}
