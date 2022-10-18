a4b9d9c4a37f0c86e33f47506d46da313891dccf
Lucille Benoit <lucillebenoit1@gmail.com>
2022-10-18 14:45:40 -0700

    merchant repository

---
+++ Sources/Catch/Utilities/Enums/NotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationName.swift
+//  
+//
+//  Created by Lucille Benoit on 10/18/22.
+//
+
+import Foundation
+
+enum NotificationName {
+    static let merchantUpdate = "MerchantUpdate"
+}
