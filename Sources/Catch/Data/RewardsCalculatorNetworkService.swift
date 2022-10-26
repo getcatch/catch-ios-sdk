4cf657fb423f33138b332728c707a860147a74ac
Lucille Benoit <lucillebenoit1@gmail.com>
2022-10-26 14:45:08 -0700

    fix typo

--- Sources/Catch/Data/RewardsCalculatorNetworkService.swift
+++ Sources/Catch/Data/RewardsCalculatorNetworkService.swift
@@ -10,16 +10,16 @@ import Foundation
 protocol RewardsCalculatorNetworkServiceInterface {
     func getCalculateEarnedRewards(merchantId: String,
                                    queryItems: [URLQueryItem]?,
-                                   completion: @escaping (Result<EarnedRewardSummary, Error>) -> Void)
+                                   completion: @escaping (Result<EarnedRewardsSummary, Error>) -> Void)
 }
 
 struct RewardsCalculatorNetworkService: RewardsCalculatorNetworkServiceInterface {
     var apiClient: APIClientInterface = APIClient()
     func getCalculateEarnedRewards(merchantId: String,
                                    queryItems: [URLQueryItem]?,
-                                   completion: @escaping (Result<EarnedRewardSummary, Error>) -> Void) {
+                                   completion: @escaping (Result<EarnedRewardsSummary, Error>) -> Void) {
         let path = String(format: CatchURL.getEarnedRewards, merchantId)
-        apiClient.fetchObject(path: path, queryItems: queryItems) { (result: Result<EarnedRewardSummary, Error>) in
+        apiClient.fetchObject(path: path, queryItems: queryItems) { (result: Result<EarnedRewardsSummary, Error>) in
             switch result {
             case .success(let earnedRewardsSummary):
                 completion(.success(earnedRewardsSummary))
