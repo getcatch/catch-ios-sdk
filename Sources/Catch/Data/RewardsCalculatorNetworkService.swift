32e98572b67aba0e98cc7f3bad8a90247aa212b7
Lucille Benoit <lucillebenoit1@gmail.com>
2022-10-26 14:31:05 -0700

    update reward model

--- Sources/Catch/Data/RewardsCalculatorNetworkService.swift
+++ Sources/Catch/Data/RewardsCalculatorNetworkService.swift
@@ -10,16 +10,16 @@ import Foundation
 protocol RewardsCalculatorNetworkServiceInterface {
     func getCalculateEarnedRewards(merchantId: String,
                                    queryItems: [URLQueryItem]?,
-                                   completion: @escaping (Result<EarnedRewardsSummary, Error>) -> Void)
+                                   completion: @escaping (Result<EarnedReward, Error>) -> Void)
 }
 
 struct RewardsCalculatorNetworkService: RewardsCalculatorNetworkServiceInterface {
     var apiClient: APIClientInterface = APIClient()
     func getCalculateEarnedRewards(merchantId: String,
                                    queryItems: [URLQueryItem]?,
-                                   completion: @escaping (Result<EarnedRewardsSummary, Error>) -> Void) {
+                                   completion: @escaping (Result<EarnedReward, Error>) -> Void) {
         let path = String(format: CatchURL.getEarnedRewards, merchantId)
-        apiClient.fetchObject(path: path, queryItems: queryItems) { (result: Result<EarnedRewardsSummary, Error>) in
+        apiClient.fetchObject(path: path, queryItems: queryItems) { (result: Result<EarnedReward, Error>) in
             switch result {
             case .success(let earnedRewardsSummary):
                 completion(.success(earnedRewardsSummary))
