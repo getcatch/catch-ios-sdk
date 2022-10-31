325e67ec5c828e8feba008f45504883bf11201c0
Lucille Benoit <lucillebenoit1@gmail.com>
2022-10-31 09:34:13 -0700

    add sign up discount

--- Sources/Catch/Models/EarnedRewardsSummary.swift
+++ Sources/Catch/Models/EarnedRewardsSummary.swift
@@ -12,6 +12,9 @@ struct EarnedRewardsSummary: Codable {
     /// Amount of rewards (in US cents) that the user can earn as a sign up bonus.
     let signUpBonusAmount: Int
 
+    /// Amount of rewards (in US cents) that the user can earn as a sign up discount.
+    let signUpDiscountAmount: Int
+
     /// Between 0 and 1, the fraction of amount paid that a user earns back from their purchase.
     let percentageRewardRate: Double
 
