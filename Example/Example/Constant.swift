2416f5b744a314ef4059ae66381d6a486ed9d114
lucillebenoit <74115740+lucillebenoit@users.noreply.github.com>
2022-11-14 09:30:22 -0800

    Price Slider and Demo Components (#40)
    * base catch widget
    
    * update base widget interface
    
    * remove demo modifying functions
    
    * purchaase confirmation widget
    
    * update tests
    
    * payment method widget
    
    * express checkout callout
    
    * campaign link
    
    * callout widget
    
    * price slider
    
    * update pbxproj

--- Example/Example/Constant.swift
+++ Example/Example/Constant.swift
@@ -5,8 +5,21 @@
 //  Created by Lucille Benoit on 9/10/22.
 //
 
-import Foundation
+import UIKit
 
 enum Constant {
     static let publicKey = "Ei6eCMabeqZT3AuiFcX3XuUC"
+    static let headerHeight: CGFloat = 60
+    static let defaultMargin: CGFloat = 12
+
+    // Colors
+    static let headerColor: UIColor? = UIColor(named: "HeadingColor")
+    static let backgroundColor: UIColor? = UIColor(named: "BackgroundColor")
+    static let catchGreen: UIColor? = UIColor(named: "CatchGreen")
+
+    // Price Slider
+    static let minPriceValue: Float = 0
+    static let maxPriceValue: Float = 500
+    static let priceSliderHeight: CGFloat = 30
+    static let sliderLabelFormat: String = "Price: $%d"
 }
---
+++ Example/Example/WidgetLabel.swift
@@ -0,0 +1,42 @@
+//
+//  WidgetLabel.swift
+//  Example
+//
+//  Created by Lucille Benoit on 11/3/22.
+//
+
+import UIKit
+
+class WidgetLabel: UIView {
+    private lazy var label: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = text
+        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let text: String
+
+    init(text: String) {
+        self.text = text
+        super.init(frame: .zero)
+        addSubview(label)
+        setConstraints()
+        backgroundColor = UIColor.systemGray6
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setConstraints() {
+        label.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constant.defaultMargin),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor),
+            label.centerYAnchor.constraint(equalTo: centerYAnchor),
+            heightAnchor.constraint(equalToConstant: Constant.headerHeight)
+        ])
+    }
+}
