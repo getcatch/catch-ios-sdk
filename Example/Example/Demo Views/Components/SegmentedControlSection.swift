9f840f0bbab26776c2400241e37bf4a88cffe6a2
Lucille Benoit <lucillebenoit1@gmail.com>
2022-11-29 16:59:28 -0800

    segmented control stack view

---
+++ Example/Example/Demo Views/Components/SegmentedControlSection.swift
@@ -0,0 +1,94 @@
+//
+//  SegmentedControlSection.swift
+//  Example
+//
+//  Created by Lucille Benoit on 11/3/22.
+//
+
+import UIKit
+
+protocol SegmentedControlDelegate: AnyObject {
+    func didSelectItem(at index: Int, sender: SegmentedControlSection)
+}
+
+/**
+A stack view which displays a title and a UISegmentedControl view horizontally.
+*/
+class SegmentedControlSection: UIView {
+    // Delegate responsible for handling didChange actions
+    weak var delegate: SegmentedControlDelegate?
+
+    // Label for the segmented control section
+    let title: String?
+
+    // Names for the segmented control items
+    let items: [String]
+
+    lazy var label = UILabel()
+    lazy var segmentedControl = UISegmentedControl(items: items)
+
+    lazy var stack: UIStackView = {
+        let stack = UIStackView()
+        stack.addArrangedSubview(label)
+        stack.addArrangedSubview(segmentedControl)
+        stack.axis = .horizontal
+        stack.distribution = .fill
+        stack.alignment = .center
+        stack.spacing = Constant.defaultMargin
+        return stack
+    }()
+
+    // MARK: - Initializers
+
+    /**
+    Initializes a view containing a labeled UISegmentedControl view
+    - Parameter title: The text to be shown to the left of the segmented control view.
+    - Parameter items: The ordered labels for each segmented control item.
+    */
+    init(title: String?, items: [String]) {
+        self.title = title
+        self.items = items
+        super.init(frame: .zero)
+        addSubview(stack)
+        configureSegmentedControl()
+        configureLabel()
+        setConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Private Helpers
+
+    private func configureLabel() {
+        label.text = title
+        label.font = Constant.bodyFont
+    }
+
+    private func configureSegmentedControl() {
+        segmentedControl.selectedSegmentTintColor = UIColor.systemGray6
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.font: Constant.bodyFont], for: .normal)
+        segmentedControl.addTarget(self, action: #selector(segmentedControlDidChange), for: .valueChanged)
+        segmentedControlDidChange(segmentedControl)
+    }
+
+    @objc
+    private func segmentedControlDidChange(_ sender: UISegmentedControl) {
+        let index = sender.selectedSegmentIndex
+        delegate?.didSelectItem(at: index, sender: self)
+    }
+
+    private func setConstraints() {
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        let constraints = [
+            stack.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stack.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stack.topAnchor.constraint(equalTo: topAnchor),
+            stack.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ]
+
+        NSLayoutConstraint.activate(constraints)
+    }
+}
