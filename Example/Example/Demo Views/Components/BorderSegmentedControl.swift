3bdff7f3de829be560715f0babfb72f91387d501
Lucille Benoit <lucillebenoit1@gmail.com>
2022-11-29 20:07:15 -0800

    widget configuration controls

--- Example/Example/Demo Views/Components/BorderSegmentedControl.swift
+++ Example/Example/Demo Views/Components/BorderSegmentedControl.swift
@@ -5,14 +5,25 @@
 //  Created by Lucille Benoit on 11/9/22.
 //
 
+import Catch
 import Foundation
 
 /**
 Segmented control view which displays the available border styles.
 */
 class BorderSegmentedControl: SegmentedControlSection {
+    let restrictOptions: Bool
+
+    var currentlySelectedStyle: BorderStyle {
+        let key = segmentedControl.titleForSegment(at: segmentedControl.selectedSegmentIndex)
+        if let key = key, let style = SegmentedControlItems.borderStyles(restricted: restrictOptions)[key] {
+            return style
+        }
+        return .roundedRect
+    }
 
     init(restrictOptions: Bool = false) {
+        self.restrictOptions = restrictOptions
         let options = SegmentedControlItems.borderStyles(restricted: restrictOptions).keys
         super.init(title: Strings.borderStyle, items: options)
     }
