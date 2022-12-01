27b01bc134c8effb61ccb83e5cde4905aeb0be48
Lucille Benoit <lucillebenoit1@gmail.com>
2022-11-30 23:23:25 -0800

    demo checkout form

---
+++ Example/Example/Demo Views/Components/LabelTextField.swift
@@ -0,0 +1,62 @@
+//
+//  LabelTextField.swift
+//  Example
+//
+//  Created by Lucille Benoit on 11/29/22.
+//
+
+import UIKit
+
+/**
+A horizontally stacked label and text field.
+*/
+class LabelTextField: UIView {
+    var text: String {
+        return textField.text ?? String()
+    }
+
+    lazy var label = UILabel(frame: .zero)
+    lazy var textField = createTextField()
+
+    lazy var stack: UIStackView = {
+        let stack = UIStackView()
+
+        stack.addArrangedSubview(label)
+        stack.addArrangedSubview(textField)
+
+        stack.axis = .horizontal
+        stack.distribution = .fill
+        stack.alignment = .fill
+        stack.spacing = Constant.configurationViewSpacing
+        return stack
+    }()
+
+    // MARK: - Initializers
+
+    init(title: String) {
+        super.init(frame: .zero)
+        configureLabel(with: title)
+        textField.setContentHuggingPriority(UILayoutPriority(249), for: .horizontal)
+        addConstrainedSubviewToView(stack)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Private Helpers
+
+    private func configureLabel(with title: String) {
+        label.text = title
+        label.font = Constant.bodyFont
+    }
+
+    private func createTextField() -> UITextField {
+        let textView = UITextField(frame: .zero)
+        textView.borderStyle = .roundedRect
+        textView.backgroundColor = .systemGray6
+        textView.textColor = .placeholderText
+        textView.font = Constant.bodyFont
+        return textView
+    }
+}
