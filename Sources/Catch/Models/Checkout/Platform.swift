b3790768afe0e8a5987d1103f75bcb9bc00b5e18
lucillebenoit <74115740+lucillebenoit@users.noreply.github.com>
2022-12-20 20:12:23 -0800

    create vcn checkout models (#61)

---
+++ Sources/Catch/Models/Checkout/Platform.swift
@@ -0,0 +1,22 @@
+//
+//  Platform.swift
+//  Catch
+//
+//  Created by Lucille Benoit on 12/13/22.
+//
+
+import Foundation
+
+public struct Platform: Codable {
+    /// Name of the platform the merchant is on.
+    let platformType: String?
+
+    /// Version of the platform the merchant is on.
+    let platformVersion: String?
+
+    /// Initializes a Platform object which contains the name and version of the platform in use.
+    public init(platformType: String?, platformVersion: String?) {
+        self.platformType = platformType
+        self.platformVersion = platformVersion
+    }
+}
---
+++ Sources/Catch/Models/Checkout/VirtualCardDetails.swift
@@ -0,0 +1,16 @@
+//
+//  VirtualCardDetails.swift
+//  Catch
+//
+//  Created by Lucille Benoit on 12/14/22.
+//
+
+import Foundation
+
+public struct VirtualCardDetails {
+    let cardNumber: String
+    let expirationYear: String
+    let expirationMonth: String
+    let cvc: String
+    let zipCode: String
+}
