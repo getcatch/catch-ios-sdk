f373803e9d5b48070e8da79839a3a5441bfea1d7
Lucille Benoit <lucillebenoit1@gmail.com>
2023-05-19 15:24:11 -0700

    color from hex update

--- Sources/Catch/Utilities/Assets/Colors/UIColor+Extension.swift
+++ Sources/Catch/Utilities/Assets/Colors/UIColor+Extension.swift
@@ -11,19 +11,15 @@ extension UIColor {
     /// Creates a Color object using a hex string and alpha value.
     convenience init(hexString: String, alpha: CGFloat = 1.0) {
         let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-        let scanner = Scanner(string: hexString)
-        if hexString.hasPrefix("#") {
-            scanner.scanLocation = 1
-        }
-        var color: UInt32 = 0
-        scanner.scanHexInt32(&color)
-        let mask = 0x000000FF
-        let redValue = Int(color >> 16) & mask
-        let greenValue = Int(color >> 8) & mask
-        let blueValue = Int(color) & mask
-        let red   = CGFloat(redValue) / 255.0
-        let green = CGFloat(greenValue) / 255.0
-        let blue  = CGFloat(blueValue) / 255.0
+        let hexValue: String = hexString.hasPrefix("#") ? String(hexString.dropFirst()) : hexString
+
+        let scanner = Scanner(string: hexValue)
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgbValue & 0x0000FF) / 255.0
         self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
