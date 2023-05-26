ef3445afd8c56a38913ca23117cb194283cc7669
lucillebenoit <74115740+lucillebenoit@users.noreply.github.com>
2023-05-25 18:12:20 -0700

    Remove unused fonts (#89)
    ### Summary
    
    Removes all CircularStd fonts that are not directly used by the SDK.

--- Sources/Catch/Utilities/Assets/Fonts/CatchFont.swift
+++ Sources/Catch/Utilities/Assets/Fonts/CatchFont.swift
@@ -9,22 +9,9 @@ import UIKit
 
 enum CatchFont {
     private enum FontName: String, CaseIterable {
-        case black = "CircularXXTT-Black"
-        case blackItalic = "CircularXXTT-BlackItalic"
         case bold = "CircularXXTT-Bold"
-        case boldItalic = "CircularXXTT-BoldItalic"
-        case book = "CircularXXTT-Book"
-        case bookItalic = "CircularXXTT-BookItalic"
-        case extraBlack = "CircularXXTT-ExtraBlack"
-        case extraBlackItalic = "CircularXXTT-ExtraBlackItalic"
-        case italic = "CircularXXTT-Italic"
-        case light = "CircularXXTT-Light"
-        case lightItalic = "CircularXXTT-LightItalic"
         case medium = "CircularXXTT-Medium"
-        case mediumItalic = "CircularXXTT-MediumItalic"
         case regular = "CircularXXTT-Regular"
-        case thin = "CircularXXTT-Thin"
-        case thinItalic = "CircularXXTT-ThinItalic"
     }
 
     static var allFontNames: [String] {
