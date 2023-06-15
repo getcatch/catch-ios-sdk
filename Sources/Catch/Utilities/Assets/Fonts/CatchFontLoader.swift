92410b5b76c218ecf11baeee6fd304048fbc093d
Lucille Benoit <lucillebenoit1@gmail.com>
2023-06-15 11:03:41 -0700

    add flexible font path

--- Sources/Catch/Utilities/Assets/Fonts/CatchFontLoader.swift
+++ Sources/Catch/Utilities/Assets/Fonts/CatchFontLoader.swift
@@ -9,7 +9,7 @@ import Foundation
 import CoreText
 
 struct CatchFontLoader {
-    static let fontPath = "Fonts/"
+    static let fontPaths = ["Fonts/", ""]
     static let fontExtension = "ttf"
 
     static func registerFonts() {
@@ -19,16 +19,18 @@ struct CatchFontLoader {
     }
 
     private static func registerFont(fontName: String) {
-        guard let fontURL = CatchResources.resourceBundle.url(
-            forResource: fontPath+fontName,
-            withExtension: fontExtension),
-              let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
-              let font = CGFont(fontDataProvider) else {
-            fatalError("Couldn't create font from filename: \(fontName) with extension \(fontExtension)")
+        for path in fontPaths {
+            if let fontURL = CatchResources.resourceBundle.url(
+                forResource: path + fontName,
+                withExtension: fontExtension),
+               let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
+               let font = CGFont(fontDataProvider)
+            {
+                var error: Unmanaged<CFError>?
+                CTFontManagerRegisterGraphicsFont(font, &error)
+                return
+            }
         }
-
-        var error: Unmanaged<CFError>?
-
-        CTFontManagerRegisterGraphicsFont(font, &error)
+        fatalError("Couldn't create font from filename: \(fontName) with extension \(fontExtension)")
     }
 }
